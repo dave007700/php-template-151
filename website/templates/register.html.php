93ca3d39c4a12cf95eed1743122aa3698071d19f
SirMorsel <patrick.nibbia@gmail.com>
2017-06-24 14:30:57 +0200

    Redesigne Register

--- website/templates/register.html.php
+++ website/templates/register.html.php
@@ -1,10 +1,47 @@
-<h1>Registrieren !!!!</h1>
+<!Doctype>
+<html>
+	<head>
 
+		<link rel="stylesheet" href="CSS/login.css">
 
+		<!-- Import Ajax From Google Servers -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<!---------------------------------------------------------------------------------------->
 
-<form action="" method="POST">
-	<input type="text" name="username" placeholder="Benutzername" value="<?= (isset($username)) ? htmlspecialchars($username) : "" ?>"></input>
-	<input type="email" name="email" placeholder="E-Mail" value="<?= (isset($email)) ? htmlspecialchars($email) : "" ?>"></input>
-	<input type="password" name="password" placeholder="Password"></input>
-	<input type="submit" value="Registrieren"></input>
-</form>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	</head>
+
+	<body>
+
+		<div class="container">
+
+      <form class="form-signin" action="" method="POST">
+        <h2 class="form-signin-heading">Registrieren</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="text" name="email" id="inputEmail" class="form-control" placeholder="Email" required="true" autofocus="" value="<?= (isset($email)) ? htmlspecialchars($email) : "" ?>">
+				<label for="inputEmail" class="sr-only">Username</label>
+        <input type="text" name="username" id="inputUsername" class="form-control" placeholder="Username" required="true" autofocus="" value="<?= (isset($username)) ? htmlspecialchars($username) : "" ?>">
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required="true">
+				<br>
+        <input class="btn btn-lg btn-success btn-block" type="submit" value="Registrieren"></input>
+				<br>
+				<a href="/login"> <button class="btn btn-lg btn-primary btn-block" type="button">Login</button> </a>
+				<br>
+				<a href="/Password/Reset"> <button class="btn btn-lg btn-warning btn-block" type="button">Passwort Vergessen</button> </a>
+				<br>
+				<a href="/ "> <button class="btn btn-lg btn-danger btn-block" type="button">Zurück</button> </a>
+      </form>
+
+    </div>
+
+	</body>
+</html>
